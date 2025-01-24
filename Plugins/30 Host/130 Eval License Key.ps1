8a68ee99058ea44bae264f842b7d64d7701ead45
ChrisLeblanc75 <chris.leblanc@dxcas.com>
2025-01-24 08:38:48 -0800

    Update 130 Eval License Key.ps1
    Eval format is different with ESXi v8

--- Plugins/30 Host/130 Eval License Key.ps1
+++ Plugins/30 Host/130 Eval License Key.ps1
@@ -3,10 +3,12 @@ $Header = "Hosts using an Evaluation Key : [count]"
 $Comments = "The following hosts are using an evaluation key that will expire, causing them to disconnect from vCenter."
 $Display = "Table"
 $Author = "Doug Taliaferro"
-$PluginVersion = 1.0
+$UpdateAuthor = "Chris Leblanc"
+$PluginVersion = 2.0
 $PluginCategory = "vSphere"
 
 # Start of Settings
 # End of Settings
 
-$VMH | where-object {($_.ConnectionState -match "Connected|Maintenance") -and ($_.LicenseKey -eq "00000-00000-00000-00000-00000")} | Select Name, LicenseKey
+#updated to check eval on ESXi v8
+$VMH | where-object {($_.ConnectionState -match "Connected|Maintenance") -and (($_.LicenseKey -eq "00000-00000-00000-00000-00000") -or ($_.LicenseKey -eq "00000-#####-#####-#####-00000"))} | Select Name, LicenseKey
